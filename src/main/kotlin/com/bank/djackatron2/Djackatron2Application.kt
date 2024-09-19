d105c4a33a7e7906ded83660b75f553fc569eb5d
Jirat Chokdeeanan <jirat.cho@gmail.com>
2024-09-19 20:37:53 +0700

    init repo

---
+++ src/main/kotlin/com/bank/djackatron2/Djackatron2Application.kt
@@ -0,0 +1,11 @@
+package com.bank.djackatron2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Djackatron2Application
+
+fun main(args: Array<String>) {
+	runApplication<Djackatron2Application>(*args)
+}
---
+++ src/test/kotlin/com/bank/djackatron2/Djackatron2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bank.djackatron2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Djackatron2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
