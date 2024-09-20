3eb5a2f80ec85a2366bf73f88ac774ea67bd75f7
Jirat Chokdeeanan <jirat.cho@gmail.com>
2024-09-20 22:00:43 +0700

    remove unused @Service

--- src/main/kotlin/com/bank/djackatron2/service/TransferService.kt
+++ src/main/kotlin/com/bank/djackatron2/service/TransferService.kt
@@ -1,9 +1,7 @@
 package com.bank.djackatron2.service
 
 import com.bank.djackatron2.domain.TransferReceipt
-import org.springframework.stereotype.Service
 
-@Service
 interface TransferService {
 
     fun transfer(amount: Double, srcAcctId: String, dstAcctId: String): TransferReceipt
