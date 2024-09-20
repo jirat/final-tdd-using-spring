4779328c66a7cb607665c98cd8a79787f7fa4403
Jirat Chokdeeanan <jirat.cho@gmail.com>
2024-09-20 21:26:20 +0700

    add h2 dependency

--- src/main/kotlin/com/bank/djackatron2/repository/AccountRepository.kt
+++ src/main/kotlin/com/bank/djackatron2/repository/AccountRepository.kt
@@ -1,7 +1,9 @@
 package com.bank.djackatron2.repository
 
 import com.bank.djackatron2.domain.Account
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AccountRepository {
     fun findById(srcAcctId: String): Account
     fun updateBalance(account: Account)
--- src/main/kotlin/com/bank/djackatron2/service/TransferService.kt
+++ src/main/kotlin/com/bank/djackatron2/service/TransferService.kt
@@ -1,7 +1,9 @@
 package com.bank.djackatron2.service
 
 import com.bank.djackatron2.domain.TransferReceipt
+import org.springframework.stereotype.Service
 
+@Service
 interface TransferService {
 
     fun transfer(amount: Double, srcAcctId: String, dstAcctId: String): TransferReceipt
